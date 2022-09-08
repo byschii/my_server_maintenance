ba363e368d69e5d521ee0f848b6e161426d45327
byschii <byschii@protonmail.com>
2022-09-08 18:44:34 +0200

    Update increase_ssh_timeout.sh

--- increase_ssh_timeout.sh
+++ increase_ssh_timeout.sh
@@ -1,3 +1,3 @@
 
-sudo echo "ClientAliveInterval" >> /etc/ssh/sshd_conf
-sudo echo "ClientAliveCountMax" >> /etc/ssh/sshd_conf
+sudo echo "ClientAliveInterval 3600" >> /etc/ssh/sshd_conf
+sudo echo "ClientAliveCountMax 3" >> /etc/ssh/sshd_conf
