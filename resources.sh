141667edc5029afb1e9ff15903457a13c6869dd8
byschii <byschii@protonmail.com>
2023-05-11 13:12:24 +0200

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -9,6 +9,6 @@ memory_usage=$(top -bn 1 -d 0 | awk '/^KiB Mem/{printf "{\"total\":%d, \"free\":
 memory_info=$(echo "$memory_usage" | jq -r '{total, free, used}')
 
 disk_usage=$( df -BG | awk '/xvda/{printf "{ \"free\":%d, \"used\":%d}", $2, $3}' )
-disk_info=$(echo "$disk_usage" | jq -r '{total, free, used}')
+disk_info=$(echo "$disk_usage" | jq -r '{ free, used}')
 
 echo "{\"datetime\":\"$datetime\", \"cpu\":$cpu_info, \"memory\":$memory_info, \"disk\":$disk_info}"
