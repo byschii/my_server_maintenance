58b5c5cbae0099446f7594adbb2e79de4739924d
byschii <byschii@protonmail.com>
2023-05-11 13:18:47 +0200

    Update resources.sh

--- resources.sh
+++ resources.sh
@@ -1,4 +1,4 @@
-sudo apt-get install jq -y
+sudo apt-get install jq -y >/dev/null 2>&1
 
 datetime=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -9,6 +9,6 @@ memory_usage=$(top -bn 1 -d 0 | awk '/^KiB Mem/{printf "{\"total\":%d, \"free\":
 memory_info=$(echo "$memory_usage" | jq -r '{total, free, used}')
 
 disk_usage=$( df -BG | awk '/xvda/{printf "{ \"free\":%d, \"used\":%d}", $2, $3}' )
-disk_info=$(echo "$disk_usage" | jq -r '{ free, used}')
+disk_info=$(echo "$disk_usage" | jq -r '{free, used}')
 
-echo "{\"datetime\":\"$datetime\", \"cpu\":$cpu_info, \"memory\":$memory_info, \"disk\":$disk_info}"
+echo "{\"datetime\":\"$datetime\", \"cpu\":$cpu_info, \"memory\":$memory_info, \"disk\":$disk_info}" | tr -d '\n' | tr -s ' '
